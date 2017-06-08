ee672c577c2035776691a05ff491e3b0c0923699
Matthew <code@kaliara.com>
2017-06-08 00:42:35 -0600

    added admin interface

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,57 @@
 $(function() {
+  // Load local ads
+  const storage = require('electron-storage');
+  const adminFile = 'admin.json';
+
+  function checkForAdminData(file) {
+    storage.isPathExists(file, (exists) => {
+      if (exists) {
+        loadAds(file);
+      }
+    });
+  }
+
+  function loadAds(file) {
+    let ads = {};
+
+    storage.get(file, (err, data) => {
+      if (err) {
+        console.error(err)
+      } else {
+        showAds(data);
+      }
+    });
+  }
+
+  function saveAds(file, ads) {
+    let jsonData = {
+      "adminData": {
+        "adUrls": {
+          "bottom": ads.bottom,
+          "side": ads.side
+        }
+      }
+    }
+
+    storage.set('admin.json', jsonData, (err) => {
+      if (err) {
+        console.error(err)
+      }
+    });
+  }
+
+  function showAds(data) {
+    setAd('bottom', data.adminData.adUrls.bottom);
+    setAd('side', data.adminData.adUrls.side);
+  }
+
+  function setAd(position, url) {
+    $("#ad_" + position).prop("src", url);
+  }
+
+  checkForAdminData(adminFile);
+
+
   // Question timing
   function toggleQuestionDisplay() {
     var current_time = parseInt($("video")[0].currentTime);
@@ -29,7 +82,7 @@ $(function() {
 
 
   // Toggle details
-  $("#details_toggle").on("click", function(e){
+  $("#details_toggle").on("click", function(e) {
     var $details = $("#details");
     var expanded = $details.hasClass("expanded");
 
@@ -41,5 +94,25 @@ $(function() {
     else {
       $(this).html("hide candidate details");
     }
+  });
+
+
+  // Toggle admin modal
+  $("#admin_show").on("click", function(e) {
+    $("#admin_modal").removeClass("hidden");
+  });
+  $("#admin_hide").on("click", function(e) {
+    $("#admin_modal").addClass("hidden");
+  });
+  $("#admin_update").on("click", function(e) {
+    var ads = {
+      bottom: $("input[name='ad_bottom']").val(),
+      side: $("input[name='ad_side']").val()
+    }
+
+    saveAds(adminFile, ads);
+    loadAds(adminFile);
+
+    $("#admin_modal").addClass("hidden");
   });
 });
